ea7719365bb42188cee1a54becffa14de30248dd
leszek-peszek <275331@student.pwr.edu.pl>
2026-05-07 16:19:48 +0200

    ups

--- python/mix.py
+++ python/mix.py
@@ -3,14 +3,14 @@ import numpy
 
 print("Hello World")
 
-def mix_TernaryEg(Eg1, Eg2, bowing,n): #Wzór A.4
-    return (x/n)*Eg1 + (1 - x/n)*Eg2 + (x/n)*(1-x/n)*bowing
+def mix_TernaryEg(Eg1, Eg2, bowing, x): #Wzór A.4
+    return (x)*Eg1 + (1 - x)*Eg2 + (x)*(1-x)*bowing
         
-def mix_QuaternaryEg1(Eg1, Eg2, Eg3, Eg4, n1, n2): # Binarka Mieszanie A z C i D oraz B z C i D, # Wzór A.6
-    return (x/n1)*(y/n2)*Eg1 + (x/n1)*(1-y/n2)*Eg2 + (1-x/n1)*(y/n2)*Eg3 + (1-x/n1)*(1-y/n2)*Eg4
+def mix_QuaternaryEg1(Eg1, Eg2, Eg3, Eg4, x, y): # Binarka Mieszanie A z C i D oraz B z C i D, # Wzór A.6
+    return (x)*(y)*Eg1 + (x)*(1-y)*Eg2 + (1-x)*(y)*Eg3 + (1-x)*(1-y)*Eg4
             
-def mix_QuaternaryEg2(Eg1, Eg2, Eg3, n1, n2): # Binarka Mieszanie A z D, B z D, C z D, Zależą wg wzoru A.8
-    return (x/n1)*Eg1 + (y/n2)*Eg2 + (1 - (x/n1) - (y/n2))*Eg3
+def mix_QuaternaryEg2(Eg1, Eg2, Eg3, x, y): # Binarka Mieszanie A z D, B z D, C z D, Zależą wg wzoru A.8
+    return (x)*Eg1 + (y)*Eg2 + (1 - (x) - (y))*Eg3
     
 def mix_QuaternaryEg3(Eg1, Eg2, Eg3, x, y): # Trójnarka Mieszanie AB z D, CB z D, AC z D,Wzór A.10 (Uwaga mieszanki muszą być faktycznymi mieszankami gdzie zmieniają parametry w zależności ile ich jest)
     return ((x*y*Eg1)+(y*(1-x-y)*Eg2)+(x*(1-x-y)*Eg3))/((x*y)+(y*(1-x-y))+(x*(1-x-y)))
